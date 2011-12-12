cb38c9342a7c84af2024c721f2649b51bd841154
David Fuhr <fuhr@flagbit.de>
2011-12-12 19:45:35 +0100

    [+FEATURE] Added openid login form

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Flagbit_OpenId
 
 Provides [OpenID](http://www.openid.net/)-Authentication for the [Magento](http://www.magentocommerce.com/) Backend.
+
+## Notes
+
+Adminhtml Controllers require a valid Session. The
+Exceptions for 'forgotpassword' and 'logout' are hardcoded in ``Mage_Admin_Model_Observer::actionPreDispatchAdmin()`` which is triggered via event defined in
+``app/code/core/Mage/Adminhtml/etc/config.xml``. This forces me to override the Observer which can cause nasty Conflicts. Any other suggestions? Anybody?
---
+++ src/app/code/community/Flagbit/OpenId/Model/Admin/Observer.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    Flagbit
+ * @package     Flagbit_OpenId
+ * @copyright   Copyright (c) 2011 Flagbit GmbH & Co. KG (http://www.flagbit.de/)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Admin observer model
+ *
+ * @category    Flagbit
+ * @package     Flagbit_OpenId
+ * @author      David Fuhr <fuhr@flagbit.de>
+ */
+class Flagbit_OpenId_Model_Admin_Observer extends Mage_Admin_Model_Observer
+{
+    public function actionPreDispatchAdmin($event)
+    {
+        $request = Mage::app()->getRequest();
+        
+        if ('admin' === $request->getModuleName() && 'openid' === $request->getControllerName() && 'login' === $request->getActionName()) {
+            $request->setDispatched(true);
+        }
+        else {
+            return parent::actionPreDispatchAdmin($event);
+        }
+        
+        /* @var $session Mage_Admin_Model_Session */
+        $session = Mage::getSingleton('admin/session');
+        $session->refreshAcl();
+    }
+}
